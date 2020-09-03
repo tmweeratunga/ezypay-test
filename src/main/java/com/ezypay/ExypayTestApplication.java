d29ed6dacd0195b2a2615375d757cb213342651f
Tharindu <tmweeratunga31@gmail.com>
2020-09-03 21:51:51 +0800

    init project with spring boot

---
+++ src/main/java/com/ezypay/ExypayTestApplication.java
@@ -0,0 +1,13 @@
+package com.ezypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExypayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExypayTestApplication.class, args);
+    }
+
+}
